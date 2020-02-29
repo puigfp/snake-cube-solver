ccc3d339ef857f3d27a3f033d85f49c64217d2a3
Francisco-Pierre Puig <puig.franciscopierre@gmail.com>
2020-02-29 18:26:11 +0100

    add rust cli interface

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,26 +1,21 @@
 use ::lib::*;
 
+use std::env;
+
 fn main() {
-    let cases = vec![
-        vec![1, 1, 1, 1, 1, 1, 1],
-        vec![2, 1, 1, 2, 1, 2, 1, 1, 2, 2, 1, 1, 1, 2, 2, 2, 2],
-        vec![
-            2, 1, 2, 1, 1, 3, 1, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2,
-            1, 1, 1, 1, 1, 2, 3, 1, 1, 1, 3, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-            3, 1,
-        ],
-    ];
-    for snake in cases.iter() {
-        for f in &[solve_naive, solve_fast] {
-            let solution = f(snake);
-            println!(
-                "{}",
-                solution
-                    .iter()
-                    .map(|direction| format!("{}", direction))
-                    .collect::<Vec<_>>()
-                    .join(",")
-            );
-        }
-    }
+    let args: Vec<String> = env::args().collect();
+    assert!(args.len() == 2);
+    let snake = args[1]
+        .split(",")
+        .map(|s| s.trim().parse().unwrap())
+        .collect::<Vec<usize>>();
+    let solution = solve_fast(&snake);
+    println!(
+        "{}",
+        solution
+            .iter()
+            .map(|direction| format!("{}", direction))
+            .collect::<Vec<_>>()
+            .join(",")
+    );
 }
